b0f5a0d39d25e998d1a093620552fe0bb6df9aea
Joao-Silva123 <100712065+Joao-Silva123@users.noreply.github.com>
2022-03-01 22:07:46 -0300

    Revert "Create vendor.rar"
    This reverts commit fb06fee2cc3e12002257114907bf095897665f1f.

--- routes/web.php
+++ routes/web.php
@@ -18,11 +18,17 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EventController;
 
 Route::get('/', [EventController::class, 'index']); 
+
 Route::get('/events/create', [EventController::class, 'create'])->middleware('auth');
+
 Route::get('/events/{id}', [EventController::class, 'show']);
+
 Route::post('/events',[EventController::class, 'store']); 
+
 Route::delete('/events/{id}', [EventController::class, 'destroy'])->middleware('auth');
+
 Route::get('/events/edit/{id}', [EventController::class, 'edit'])->middleware('auth');
+
 Route::put('/events/update/{id}', [EventController::class, 'update'])->middleware('auth');
 
 Route::get('/contact', function () {
